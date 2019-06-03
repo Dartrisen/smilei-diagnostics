12a76f60ee2fe06351b45c8c5f8071f0278c913e
Dartrisen <calculating.scientist@gmail.com>
2019-06-03 13:25:31 +0200

    Update field.py
    correct s_field in development

--- field.py
+++ field.py
@@ -6,8 +6,9 @@ class Field:
         import happi
         from numpy import pi
 
-        self.value = happi.Open(data)
-        self.L0 = 2 * pi
+        self.value  = happi.Open(data)
+        self.L0     = 2 * pi
+        self.resx   = 50
 
     @property
     def eons(self):
@@ -18,16 +19,20 @@ class Field:
         return self.value.Field(0, 'Rho_ion')
 
     def get_e_field(self, time, axis='x'):
-        if direction in ['x', 'y', 'z']:
-            return self.value.Field(0, f'E{axis}').getData(timestep=time)
+        if axis in ['x', 'y', 'z']:
+            return self.value.Field(0, f'E{axis}').getData(timestep=time)[0]
 
     def get_b_field(self, time, axis='x'):
-        if direction in ['x', 'y', 'z']:
-            return self.value.Field(0, f'B{axis}').getData(timestep=time)
+        if axis in ['x', 'y', 'z']:
+            return self.value.Field(0, f'B{axis}').getData(timestep=time)[0]
 
     @property
     def x(self):
         return self.eons.getAxis(axis='x')/self.L0
+
+    @property
+    def y(self):
+        return self.eons.getAxis(axis='y')/self.L0
 
     @staticmethod
     def plot(x, y, m_dict):
@@ -53,8 +58,8 @@ class Field:
     def plot_ne(self, time):
         print('plot the ne electron density distribution')
 
-        ne = self.eons.getData(timestep=time)[0]
-        x = self.x
+        ne     = self.eons.getData(timestep=time)[0]
+        x      = self.x
 
         m_dict = {'label'   :'ne',
                   'xmin'    : None,
@@ -67,7 +72,126 @@ class Field:
 
         self.plot(x, ne, m_dict)
 
+    def plot_ni(self, time):
+        print('plot the ni electron density distribution')
+
+        ni     = self.ions.getData(timestep=time)[0]
+        x      = self.x
+
+        m_dict = {'label'   :'ni',
+                  'xmin'    : None,
+                  'xmax'    : None,
+                  'xlabel'  : r'$\mathrm{x \ [\lambda_0]}$',
+                  'ymin'    : None,
+                  'ymax'    : None,
+                  'ylabel'  : r'$\mathrm{ni \ [n_c]}$',
+                  'name'    : f'ni_{time}.png'}
+
+        self.plot(x, ne, m_dict)
+
+
+    def plot_ne_fft(self, time):
+        from numpy import where
+
+        print('plot the fft ne electron density spectr near nc/4')
+
+        ne     = self.eons.getData(timestep=time)[0]
+        n0     = self.eons.getData(timestep=0)[0]
+        x      = self.x
+
+
+        idx    = where((x>110) & (x<125))
+        ne     = ne[idx]
+        x      = x[idx]
+
+        ne_fft = fft(ne-n0)
+        kx     = 50*fftfreq(x.shape[0])
+
+        m_dict = {'label'   :'ne',
+                  'xmin'    : None,
+                  'xmax'    : None,
+                  'xlabel'  : r'$\mathrm{kx \ [\lambda_0]}$',
+                  'ymin'    : None,
+                  'ymax'    : None,
+                  'ylabel'  : r'$\mathrm{ne \ [n_c]}$',
+                  'name'    : f'ne_fft_{time}.png'}
+
+        self.plot(kx, ne_fft, m_dict)
+
+    def plot_e_field(self, time, axis='y'):
+        from numpy import where
+
+        print(f'plot the e{axis} field near nc/4')
+
+        e_f    = self.get_e_field(time=time, axis=axis)
+        x      = self.x
+
+        idx    = where((x>110) & (x<125))
+        e_f    = e_f[idx]
+        x      = x[idx]
+
+        m_dict = {'label'   :f'e{axis}',
+                  'xmin'    : None,
+                  'xmax'    : None,
+                  'xlabel'  : r'$\mathrm{x \ [\lambda_0]}$',
+                  'ymin'    : None,
+                  'ymax'    : None,
+                  'ylabel'  : f'e{axis}' + r'$\mathrm{ \ [m_e c \omega / e]}$',
+                  'name'    : f'e{axis}_{time}.png'}
+
+        self.plot(x, e_f, m_dict)
+
+    def plot_b_field(self, time, axis='z'):
+        from numpy import where
+
+        print(f'plot the b{axis} field near nc/4')
+
+        b_f    = self.get_b_field(time=time, axis=axis)
+        x      = self.x
+
+        idx    = where((x>110) & (x<125))
+        b_f    = b_f[idx]
+        x      = x[idx]
+
+        m_dict = {'label'   :f'b{axis}',
+                  'xmin'    : None,
+                  'xmax'    : None,
+                  'xlabel'  : r'$\mathrm{x \ [\lambda_0]}$',
+                  'ymin'    : None,
+                  'ymax'    : None,
+                  'ylabel'  : f'b{axis}' + r'$\mathrm{ \ [m_e c \omega / e]}$',
+                  'name'    : f'b{axis}_{time}.png'}
+
+        self.plot(x, b_f, m_dict)
+
+    def plot_sx(self, time, axis='x'):
+        from numpy import where
+
+        print(f'plot the s{axis} poynting vector')
+
+        e_f    = lambda ax: self.get_e_field(time=time, axis=ax)
+        b_f    = lambda ax: self.get_b_field(time=time, axis=ax)
+        s_f    = e_f('y')*b_f('z') - e_f('z')*b_f('y')
+        x      = self.x
+
+        idx    = where((x>0) & (x<25))
+        s_f    = s_f[idx]
+        x      = x[idx]
+
+        m_dict = {'label'   :f's{axis}',
+                  'xmin'    : None,
+                  'xmax'    : None,
+                  'xlabel'  : r'$\mathrm{x \ [\lambda_0]}$',
+                  'ymin'    : None,
+                  'ymax'    : None,
+                  'ylabel'  : f's{axis}' + r'$\mathrm{ \ [c (m_e \omega / e)^2]}$',
+                  'name'    : f's{axis}_{time}.png'}
+
+        self.plot(x, s_f, m_dict)
+
 
 if __name__ == '__main__':
-    f = Field('1D_test/')
-    f.plot_ne(10000)
+    f = Field('../1D_test/')
+    f.plot_e_field(10000, 'y')
+    f.plot_b_field(10000, 'z')
+  
